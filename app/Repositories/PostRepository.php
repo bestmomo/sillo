d50dce2118cbdb11df1df0aa450ec00645667a19
GrCOTE7 <grcote7@gmail.com>
2024-06-11 19:42:47 +0200

    Fix: Specific request part to authorize PostgresSql too

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -1,117 +1,114 @@
 <?php
 
+/**
+ * (ɔ) LARAVEL.Sillo.org - 2015-2024
+ */
+
 namespace App\Repositories;
 
-use App\Models\{Post, Category, Serie};
-use Illuminate\Pagination\LengthAwarePaginator;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Serie;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class PostRepository
 {
-    /**
-     * Récupère la requête de base pour les posts.
-     *
-     * @return Builder
-     */
-    protected function getBaseQuery(): Builder
-    {
-        $adaptedReqForSqliteOrMysql = (env('DB_CONNECTION') == "mysql") ? "LEFT(body, LOCATE(' ', body, 300))" : "substr(body, 1, instr(substr(body, 300), ' '))";
+	/**
+	 * Récupère les posts paginés en fonction de la catégorie ou de la série.
+	 */
+	public function getPostsPaginate(?Category $category, ?Serie $serie): LengthAwarePaginator
+	{
+		$query = $this->getBaseQuery();
 
-        return Post::select(
-            'id',
-            'slug',
-            'image',
-            'title',
-            'user_id',
-            'category_id',
-            'serie_id',
-            'created_at'
-        )
-                        ->selectRaw("
+		if ($category) {
+			$query->whereBelongsTo($category);
+		}
+
+		if ($serie) {
+			$query->whereBelongsTo($serie)->oldest();
+		}
+
+		return $query->paginate(config('app.pagination'));
+	}
+
+	/**
+	 * Récupère un post par son slug.
+	 */
+	public function getPostBySlug(string $slug): Post
+	{
+		$post = Post::with('user:id,name', 'category', 'serie')
+			->withCount('validComments')
+			->whereSlug($slug)
+			->firstOrFail();
+
+		if ($post->serie_id) {
+			$post->previous = $post->parent_id ? Post::findOrFail($post->parent_id) : null;
+			$post->next     = Post::whereParentId($post->id)->first() ?: null;
+		}
+
+		return $post;
+	}
+
+	/**
+	 * Recherche les posts en fonction d'un terme de recherche.
+	 */
+	public function search(string $search): LengthAwarePaginator
+	{
+		return $this->getBaseQuery()
+			->where(function ($query) use ($search) {
+				$query->where('body', 'like', "%{$search}%")
+					->orWhere('title', 'like', "%{$search}%");
+			})
+			->paginate(config('app.pagination'));
+	}
+
+	/**
+	 * Génère un slug unique pour un post.
+	 */
+	public function generateUniqueSlug(string $slug): string
+	{
+		$newSlug = $slug;
+		$counter = 1;
+		while (Post::where('slug', $newSlug)->exists()) {
+			$newSlug = $slug . '-' . $counter;
+			++$counter;
+		}
+
+		return $newSlug;
+	}
+
+	/**
+	 * Récupère la requête de base pour les posts.
+	 */
+	protected function getBaseQuery(): Builder
+	{
+		$specificReq = [
+			'mysql'  => "LEFT(body, LOCATE(' ', body, 300))",
+			'sqlite' => "substr(body, 1, instr(substr(body, 300), ' '))",
+			'pgsql'  => "substring(body from '^.{1,300}\\b')",
+		];
+		$usedDbSystem = env('DB_CONNECTION', 'mysql');
+		$adaptedReq   = $specificReq[$usedDbSystem];
+
+		return Post::select(
+			'id',
+			'slug',
+			'image',
+			'title',
+			'user_id',
+			'category_id',
+			'serie_id',
+			'created_at'
+		)
+			->selectRaw("
                                 CASE
                                     WHEN LENGTH(body) <= 300 THEN body
-                                    ELSE $adaptedReqForSqliteOrMysql 
+                                    ELSE {$adaptedReq}
                                 END AS excerpt
                             ")
-                        ->with('user:id,name', 'category', 'serie')
-                        ->whereActive(true)
-                        ->latest();
-
-    }
-
-    /**
-     * Récupère les posts paginés en fonction de la catégorie ou de la série.
-     *
-     * @param Category|null $category
-     * @param Serie|null $serie
-     * @return LengthAwarePaginator
-     */
-    public function getPostsPaginate(?Category $category, ?Serie $serie): LengthAwarePaginator
-    {
-        $query = $this->getBaseQuery();
-
-        if ($category) {
-            $query->whereBelongsTo($category);
-        }
-
-        if ($serie) {
-            $query->whereBelongsTo($serie)->oldest();
-        }
-
-        return $query->paginate(config('app.pagination'));
-    }
-
-    /**
-     * Récupère un post par son slug.
-     *
-     * @param string $slug
-     * @return Post
-     */
-    public function getPostBySlug(string $slug): Post
-    {
-        $post = Post::with('user:id,name', 'category', 'serie')
-                    ->withCount('validComments')
-                    ->whereSlug($slug)
-                    ->firstOrFail();
-
-        if ($post->serie_id) {
-            $post->previous = $post->parent_id ? Post::findOrFail($post->parent_id) : null;
-            $post->next = Post::whereParentId($post->id)->first() ?: null;
-        }
-
-        return $post;
-    }
-
-    /**
-     * Recherche les posts en fonction d'un terme de recherche.
-     *
-     * @param string $search
-     * @return LengthAwarePaginator
-     */
-    public function search(string $search): LengthAwarePaginator
-    {
-        return $this->getBaseQuery()
-                    ->where(function ($query) use ($search) {
-                        $query->where('body', 'like', "%$search%")
-                              ->orWhere('title', 'like', "%$search%");
-                    })
-                    ->paginate(config('app.pagination'));
-    }
-
-    /**
-     * Génère un slug unique pour un post.
-     *
-     * @param string $slug
-     * @return string
-     */
-    public function generateUniqueSlug(string $slug): string
-    {
-        $newSlug = $slug;
-        $counter = 1;
-        while (Post::where('slug', $newSlug)->exists()) {
-            $newSlug = $slug . '-' . $counter;
-            $counter++;
-        }
-        return $newSlug;
-    }
+			->with('user:id,name', 'category', 'serie')
+			->whereActive(true)
+			->latest();
+	}
 }
