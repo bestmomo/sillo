e8bbb25fbbb9a78e7ded5039d587fa5631dbcd1a
GrCOTE7 <grcote7@gmail.com>
2024-06-16 09:14:42 +0200

    Feat: Upgrade counter(by)

--- resources/views/livewire/gc7/abc/livewire/counter.blade.php
+++ resources/views/livewire/gc7/abc/livewire/counter.blade.php
@@ -3,15 +3,15 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    public $count = 0;
+    public $count = 10;
     public $by;
     public function mount($by = 1)
     {
         $this->by = (int) $by;
     }
-    public function increment()
+    public function increment($by = 1)
     {
-        $this->count += $this->by;
+        $this->count += $by;
     }
     public function decrement()
     {
@@ -23,9 +23,10 @@ new class extends Component {
     <p>
         Counter: {{ $count }}<br>
         {{-- <x-button class="btn-primary mt-1" wire:click="increment">+</x-button> --}}
+        {{-- <x-button class="btn-primary mt-1" wire:mouseenter="increment">+</x-button> --}}
         {{-- <x-button class="btn-primary mt-1" wire:click.window="increment">+</x-button> --}}
         {{-- <x-button class="btn-primary mt-1" wire:click.throttle.3000ms="increment">+</x-button> --}}
-        <x-button class="btn-primary mt-1" wire:click.debounce.0ms="increment()">+</x-button>
+        <x-button class="btn-primary mt-1" wire:click.debounce.0ms="increment(5)">+</x-button>
         <x-button class="btn-primary mt-1" wire:click.debounce.0ms="decrement()">-</x-button>
     </p>
 </div>
