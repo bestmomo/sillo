f4749eab789ef48198f189022c4dcfa308494511
GrCOTE7 <grcote7@gmail.com>
2024-06-16 10:17:13 +0200

    Feat: Task add with alert

--- resources/views/livewire/gc7/abc/livewire/todos.blade.php
+++ resources/views/livewire/gc7/abc/livewire/todos.blade.php
@@ -1,8 +1,13 @@
 <?php
 
 use Livewire\Volt\Component;
+use Livewire\Attributes\Rule;
+use Mary\Traits\Toast;
 
 new class extends Component {
+    use Toast;
+
+    #[Rule('required')]
     public $todo = '';
     public $count = 0;
     public $todos = ['Take out trash', 'Do dishes'];
@@ -14,9 +19,12 @@ new class extends Component {
 
     public function add()
     {
+        $this->validate();
+
         $this->todos[] = ucfirst($this->todo);
         $this->reset('todo');
         $this->reset('count');
+        $this->success('Done task added');
     }
 }; ?>
 
@@ -24,10 +32,10 @@ new class extends Component {
     <h2>Todos</h2>
 
     <form wire:submit='add'>
-        <div class="flex items-center">
-            <x-textarea type="text" wire:model="todo" wire:keyup="increment" placeholder="New Task here"
-                hint='Count: {{ $this->count }}' focus></x-textarea>
-            <x-button class="btn-primary ml-3" type="submit">Add</x-button>
+        <div class="flex items-end my-3">
+            <x-input type="text" wire:model="todo" wire:keyup="increment" focus></x-input>
+            <x-button class="btn-primary ml-3" type="submit" icon="o-bars-arrow-up" spiner>Add</x-button>
+            <span class='ml-3'>Count: {{ $this->count }}</span>
         </div>
     </form>
 
