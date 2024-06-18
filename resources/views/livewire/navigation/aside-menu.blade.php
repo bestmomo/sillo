106420d84117ff95a8fbe24033187dc4772d9132
GrCOTE7 <grcote7@gmail.com>
2024-06-18 08:51:04 +0200

    Fix: Remove unused code

--- resources/views/livewire/navigation/aside-menu.blade.php
+++ resources/views/livewire/navigation/aside-menu.blade.php
@@ -7,8 +7,7 @@
 
         @foreach ($links as $link)
             <div class="my-5">
-                <a href="/t/{{ strtolower($link) }}" :class="{ 'current': true }"
-                    class="py-1 px-2 rounded my-5 {{ request()->is('t/' . strtolower($link)) ? 'bg-gray-700 text-white' : '' }}">{{ ucfirst(str_replace('-', ' ',$link)) }}</a>
+                <a href="/t/{{ strtolower($link) }}" class="py-1 px-2 rounded my-5 {{ request()->is('t/' . strtolower($link)) ? 'bg-gray-700 text-white' : '' }}">{{ ucfirst(str_replace('-', ' ',$link)) }}</a>
             </div>
         @endforeach
 
