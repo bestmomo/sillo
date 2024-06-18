2eee4c9b837cff658fb97563a255f13a8844fa04
GrCOTE7 <grcote7@gmail.com>
2024-06-18 14:22:01 +0200

    Feat: New form with count words

--- resources/views/livewire/gc7/abc/livewire/new-form.blade.php
+++ resources/views/livewire/gc7/abc/livewire/new-form.blade.php
@@ -3,19 +3,24 @@ include_once 'new-form.php';
 ?>
 
 <div>
-    <x-header title="New Form" shadow separator progress-indicator>
-    </x-header>
-
-    <div x-data="{ count: 7 }">
-        <span x-text="count"></span>
-        <button x-on:click="count++">Add</button>
-    </div>
-
-    Current title: <span x-text="$wire.title.toUpperCase()"></span>
 
     @php
         $maxChars = env('APP_MAX_NUMBER_OF_CHARS_IN_COMMENTS_FORM');
     @endphp
+
+    <x-header title="New Form" shadow separator progress-indicator>
+    </x-header>
+    
+    {{-- DOC --}}
+    {{--
+    <div x-data="{ count: {{ $maxChars }} }">
+        <span x-text="count"></span>
+        <x-button class='btn-primary ml-3' x-on:click="count++">Add</x-button>
+    </div>
+    Current title: <span x-text="$wire.title.toUpperCase()"></span> 
+    <x-button class='btn-primary ml-3' x-on:click="$wire.title=''">Clear title</x-button>
+    <x-button type="button" class='btn-primary ml-3' x-on:click="$wire.save()">Submit form</x-button>
+    --}}
 
     <form wire:submit="save">
 
@@ -29,10 +34,14 @@ include_once 'new-form.php';
             <x-textarea class="mt-1" name="" id="" cols="30" rows="7" wire:model='content'
                 maxlength="{{ $maxChars }}"></x-textarea>
         </label>
+        <div class="text-right italic">
+            <small>Characters: <span x-text="$wire.content.length + ' / ' + {{ $maxChars }}"></span> -
+                Words: <span x-text="$wire.content.split(' ').length -1"></span>
+            </small>
+        </div>
 
         <div class="text-right w-full">
-            <x-button type="submit" class="btn-primary mt-2 mr-5">
-                Save</x-button>
+            <x-button type="submit" class="btn-primary mt-2 mr-5">Save</x-button>
         </div>
     </form>
 </div>
