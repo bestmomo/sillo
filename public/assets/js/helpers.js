1d1a01aa2004c00e3282f325a037a5faeee906e7
GrCOTE7 <grcote7@gmail.com>
2024-06-20 10:52:55 +0200

    Feat: JS in separate file called from View

---
+++ public/assets/js/helpers.js
@@ -0,0 +1,4 @@
+function ucFirst(string) {
+  console.log('New Function ucFirst');
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- resources/views/livewire/gc7/frameworks/alpinejs/pets.blade.php
+++ resources/views/livewire/gc7/frameworks/alpinejs/pets.blade.php
@@ -1,91 +1,63 @@
 <?php
 include_once 'pets.php';
+// https://learnwebcode.github.io/json-example/pets-data.json
 ?>
-
 <div>
+    <script src="/assets/js/helpers.js"></script>
     @section('styles')
         <style>
             {{ file_get_contents(resource_path('views/livewire/gc7/frameworks/alpinejs/pets/styles.css')) }}
         </style>
     @endsection
 
-    <div class="all-pets flex flex-column mx-auto max-w-[700px] justify-center mt-5">
-        <div class="pet-card card card-side bg-base-100 mb-3 max-y-[100] overflow-hidden h-[200px]">
-            <div class="flex">
-                {{-- <figure class="pet-image"> --}}
-                <img class="w-[300px] object-cover" src="https://learnwebcode.github.io/json-example/images/dog-1.jpg"
-                    alt="dog1">
-                {{-- </figure> --}}
-                <div class="pet-text card-body w-[400px] pt-5">
-                    <h2 class="card-title">NAME Here</h2>
-                    <p class="my-0 py-0" class="specie">Species: Dog</p>
-                    <p class="food my-0 py-0">Favorite food: Carrots, celery, fish</p>
-                    <div class="card-actions">
-                        <p>
-                            <button class="cool-button btn btn-primary btn-xs">Toggle Age
-                            </button>
-                        </p>
-                        <p>
-                            <button class="btn btn-error delete-pet btn-xs">Delete Pet
-                            </button>
-                        </p>
+
+    <div x-data="{
+        pets: [],
+        ucfirst: window.ucFirst
+    }" x-init="{ pets } = await (await fetch('https://learnwebcode.github.io/json-example/pets-data.json')).json()">
+
+        <template class="all-pets flex flex-column mx-auto max-w-[700px] justify-center mt-5" x-for="pet in pets">
+
+            <div class="pet-card card bg-base-100 mb-3 max-y-[100] overflow-hidden h-[200px]">
+                <div class="flex">
+
+                    {{-- <figure class="pet-image"> --}}
+                    {{-- <img class="w-[300px] object-cover" src='<span x-text="pet.photo"></span>' alt="dog1"> --}}
+                    {{-- </figure> --}}
+
+                    <div class="pet-text card-body w-full sm:w-[400px] pt-5">
+
+                        {{-- NAME --}}
+                        <div class="card-title text-xs sm:text-base"><strong x-text="pet.name"></strong></div>
+
+                        {{-- SPECIES --}}
+                        <p class="specie my-0 py-0 text-xs sm:text-base"><span x-text="pet.species"></span></p>
+
+                        {{-- FOOD --}}
+                        <template x-if="pet.favFoods">
+                            <p class="food my-0 py-0 text-xs sm:text-base overflow-auto  whitespace-normal">Favorite
+                                foods:
+                                <span x-html="ucfirst(pet.favFoods.join(', '))"></span>
+                            </p>
+                        </template>
+
+                        {{-- ACTION --}}
+                        <div class="card-actions">
+                            <p>
+                                <button class="cool-button btn btn-primary btn-xs">Toggle Age
+                                </button>
+                            </p>
+                            <p>
+                                <button class="btn btn-error delete-pet btn-xs">Delete Pet
+                                </button>
+                            </p>
+                        </div>
                     </div>
+
                 </div>
             </div>
-        </div>
+
+        </template>
+
     </div>
-
-    <div class="all-pets flex flex-column mx-auto max-w-[700px] justify-center mt-5">
-        <div class="pet-card card card-side bg-base-100 mb-3 max-y-[100] overflow-hidden h-[200px]">
-            <div class="flex">
-                {{-- <figure class="pet-image"> --}}
-                <img class="w-[300px] object-cover" src="https://learnwebcode.github.io/json-example/images/dog-1.jpg"
-                    alt="dog1">
-                {{-- </figure> --}}
-                <div class="pet-text card-body w-[400px] pt-5">
-                    <h2 class="card-title">NAME Here</h2>
-                    <p class="my-0 py-0" class="specie">Species: Dog</p>
-                    <p class="food my-0 py-0">Favorite food: Carrots, celery, fish</p>
-                    <div class="card-actions">
-                        <p>
-                            <button class="cool-button btn btn-primary btn-xs">Toggle Age
-                            </button>
-                        </p>
-                        <p>
-                            <button class="btn btn-error delete-pet btn-xs">Delete Pet
-                            </button>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="all-pets flex flex-column mx-auto max-w-[700px] justify-center mt-5">
-        <div class="pet-card card card-side bg-base-100 mb-3 max-y-[100] overflow-hidden h-[200px]">
-            <div class="flex">
-                {{-- <figure class="pet-image"> --}}
-                <img class="w-[300px] object-cover" src="https://learnwebcode.github.io/json-example/images/dog-1.jpg"
-                    alt="dog1">
-                {{-- </figure> --}}
-                <div class="pet-text card-body w-[400px] pt-5">
-                    <h2 class="card-title">NAME Here</h2>
-                    <p class="my-0 py-0" class="specie">Species: Dog</p>
-                    <p class="food my-0 py-0">Favorite food: Carrots, celery, fish</p>
-                    <div class="card-actions">
-                        <p>
-                            <button class="cool-button btn btn-primary btn-xs">Toggle Age
-                            </button>
-                        </p>
-                        <p>
-                            <button class="btn btn-error delete-pet btn-xs">Delete Pet
-                            </button>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
 </div>
