325392d8f34cefce41828758b90674483f87b5c7
GrCOTE7 <grcote7@gmail.com>
2024-06-22 18:46:42 +0200

    Feat: Non stop propagation on click

--- resources/views/livewire/gc7/frameworks/alpinejs/ga/01_bases.blade.php
+++ resources/views/livewire/gc7/frameworks/alpinejs/ga/01_bases.blade.php
@@ -4,30 +4,38 @@
 
     <div x-data="{ showSpoiler: false }">
 
-        <x-button class='btn-primary w-[150px]' x-on:click="showSpoiler = !showSpoiler"><span
+        <span @click.window="if (showSpoiler) showSpoiler = false"></span>
+
+        <x-button class='btn-primary w-[150px]' x-on:click.stop="showSpoiler = !showSpoiler"><span
                 x-text="!showSpoiler ? 'Montrer le spoiler':'Cacher le spoiler'"></span></x-button>
 
         <button class='btn btn-primary ml-2' x-transition.duration.2000ms x-show="!showSpoiler"
-            @click="showSpoiler = true">Afficher le spoiler</button>
+            @click="showSpoiler = true">Afficher le spoiler (No .stop)</button>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur suscipit repellendus ipsam dicta, ipsum
-            illo culpa deleniti laborum!</p>
+        <div class="text-justify">
 
-        <p>Porro iste nihil cum assumenda blanditiis molestias voluptates voluptate dignissimos explicabo ab
-            repudiandae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur suscipit repellendus ipsam dicta,
+                ipsum
+                illo culpa deleniti laborum!</p>
 
-        <p x-cloak x-transition.duration.2000ms x-show="showSpoiler"><strong>V 1: Ce paragraphe spoile le
-                film...</strong> Porro vitae hic est tenetur ipsa ab a unde. Commodi sequi eum quasi quisquam quas
-            aperiam!</p>
+            <p>Porro iste nihil cum assumenda blanditiis molestias voluptates voluptate dignissimos explicabo ab
+                repudiandae?</p>
 
-        <template x-if="showSpoiler">
-            <p><strong>V 2: Ce paragraphe spoile le film...</strong> Porro vitae hic est tenetur ipsa ab a unde. Commodi
-                sequi eum quasi quisquam quas aperiam!</p>
-        </template>
+            <p x-cloak x-transition.duration.2000ms x-show="showSpoiler"><strong>V 1: Ce paragraphe spoile le
+                    film...</strong> Porro vitae hic est tenetur ipsa ab a unde. Commodi sequi eum quasi quisquam quas
+                aperiam!</p>
+
+            <template x-if="showSpoiler">
+                <p><strong>V 2: Ce paragraphe spoile le film...</strong> Porro vitae hic est tenetur ipsa ab a unde.
+                    Commodi
+                    sequi eum quasi quisquam quas aperiam!</p>
+            </template>
+            
+        </div>
 
         <button
             class='btn btn-outline btn-block text-orange-500 hover:bg-orange-500 hover:text-black glass transition duration-[500ms] ease-in-out'
-            x-on:click="showSpoiler = true" x-transition.duration.2000ms x-show="!showSpoiler">Show Spoiler</button>
+            x-on:click.stop="showSpoiler = true" x-transition.duration.2000ms x-show="!showSpoiler">Show Spoiler</button>
 
     </div>
 
