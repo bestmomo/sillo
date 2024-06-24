25ded8590562be3d93af0ab0dbdb291d25b81230
GrCOTE7 <grcote7@gmail.com>
2024-06-24 10:38:13 +0200

    Feat: Add glass effect

--- resources/views/livewire/gc7/frameworks/alpinejs/ga/02_bases.blade.php
+++ resources/views/livewire/gc7/frameworks/alpinejs/ga/02_bases.blade.php
@@ -7,20 +7,23 @@
     @endsection
 
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            Alpine.data("tabs", (defaultTab) => ({
-                tab: defaultTab,
-                toggleTab(e) {
-                    this.tab = e.target.id;
-                    console.log('tab active: ' + this.tab)
-                    console.log(this.isActive(this.tab));
-                },
-                isActive(tab) {
-                    return this.tab === tab;
-                },
-            }));
+        // document.addEventListener("DOMContentLoaded", () => {
+            document.addEventListener("alpine:init", () => {
 
-        });
+                Alpine.data("tabs", (defaultTab) => ({
+                    tab: defaultTab,
+                    toggleTab(e) {
+                        this.tab = e.target.id;
+                        console.log('tab active: ' + this.tab)
+                        console.log(this.isActive(this.tab));
+                    },
+                    isActive(tab) {
+                        return this.tab === tab;
+                    },
+                }));
+
+            });
+        // });
     </script>
 
 
@@ -31,24 +34,29 @@
         <div role="tablist" class="tabs tabs-lifted mt-3 border-b border-orange-400">
 
             <button id='tab1' type="radio" role="tab"
-                class="tab brdb0 [--tab-border-color:orange-400] border-orange-400 font-bold text-orange-400" @click="toggleTab($event)" :class="{ 'glass': isActive('tab1'), 'tab-active': isActive('tab1')}">Tab 1</button>
+                class="tab brdb0 [--tab-border-color:orange-400] border-orange-400 font-bold text-orange-400"
+                @click="toggleTab($event)" :class="{ 'glass': isActive('tab1'), 'tab-active': isActive('tab1') }">Tab
+                1</button>
 
             <button id='tab2' type="radio" role="tab"
-                class="tab brdb0 [--tab-border-color:orange-400] border-orange-400 font-bold text-orange-400" @click="toggleTab($event)" :class="{ 'glass': isActive('tab2'), 'tab-active': isActive('tab2')}">Tab 2</button>
+                class="tab brdb0 [--tab-border-color:orange-400] border-orange-400 font-bold text-orange-400"
+                @click="toggleTab($event)" :class="{ 'glass': isActive('tab2'), 'tab-active': isActive('tab2') }">Tab
+                2</button>
 
             <button id='tab3' type="radio" role="tab"
-                class="tab brdb0 [--tab-border-color:orange-400] border-orange-400 font-bold text-orange-400"@click="toggleTab($event)" :class="{ 'glass': isActive('tab3'), 'tab-active': isActive('tab3')}">Tab 3</button>
+                class="tab brdb0 [--tab-border-color:orange-400] border-orange-400 font-bold text-orange-400"@click="toggleTab($event)"
+                :class="{ 'glass': isActive('tab3'), 'tab-active': isActive('tab3') }">Tab 3</button>
         </div>
 
-        <div>
+        <div class="bg-gray-700 glass rounded-b-box mt-3 text-white text-black text-justify">
 
             <template x-if="isActive('tab1')">
-                <div role="tabpanel" class="border-base-300 rounded-box p-6">Tab content 1
+                <div role="tabpanel" class="p-6">Tab content 1
                 </div>
             </template>
 
             <template x-if="isActive('tab2')">
-                <div role="tabpanel" class="border-base-300 rounded-box p-6">
+                <div role="tabpanel" class="p-6">
                     <h2>Onglet 2</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, libero saepe exercitationem
                         ducimus dicta cumque nemo, ab esse itaque deleniti sunt atque sequi repudiandae, architecto
@@ -57,7 +65,7 @@
             </template>
 
             <template x-if="isActive('tab3')">
-                <div role="tabpanel" class="border-orange-400 rounded-box p-6 text-justify">
+                <div role="tabpanel" class="p-6">
                     <h2>Onglet 3</h2>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus assumenda harum doloribus
                         ea
@@ -70,7 +78,4 @@
         </div>
     </div>
 
-    {{-- @section('scripts')
-        <script src="/assets/js/ga/02_app.js"></script>
-    @endsection --}}
 </div>
