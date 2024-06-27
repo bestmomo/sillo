5b8e34ab5c1b0df602d0054190a345501488837d
GrCOTE7 <grcote7@gmail.com>
2024-06-28 00:30:48 +0200

    Feat: Reset Test component

--- resources/views/livewire/gc7/abc/aa_test.php
+++ resources/views/livewire/gc7/abc/aa_test.php
@@ -4,56 +4,22 @@
  * (ɔ) LARAVEL.Sillo.org - 2015-2024
  */
 
-use App\Services\Gc7FrameworksLinksService;
-use Illuminate\Support\Facades\Route;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Volt\Component;
-use Spatie\Sitemap\Sitemap;
-use Spatie\Sitemap\Tags\Url;
 
 new
-#[Layout('components.layouts.gc7.main')]
 #[Title('Test')]
-class extends Component {
-	public $links = [];
-
-	public function mount(): void
+#[Layout('components.layouts.gc7.main')]
+class() extends Component {
+	public function mount($loadMore = true, $offset = 0)
 	{
-		$this->links = app(Gc7FrameworksLinksService::class)->getLinks();
-
-		$paths = $this->getPathsRoutes();
-		
-		$this->createSitemap();
-		
-		
-
-		// dd($this->links);
+		//
 	}
-
-	public function createSitemap()
-	{
-		$routeCollection = Route::getRoutes();
-
-		$sitemap = Sitemap::create();
-
-		foreach ($routeCollection as $route) {
-			$sitemap->add(Url::create($route->uri()));
-		}
-
-		$sitemap->writeToFile(public_path('complete_sitemap.xml'));
-	}
-
-	public function getPathsRoutes()
-	{
-		$routeCollection = Route::getRoutes();
-
-		$paths = [];
-
-		foreach ($routeCollection as $route) {
-			// $paths[] = $route->getPath();
-		}
-
-		return $paths;
+	public function with(): mixed
+	{		
+		//
+		return [
+		];
 	}
 };
--- resources/views/livewire/gc7/abc/aaa_test.blade.php
+++ resources/views/livewire/gc7/abc/aaa_test.blade.php
@@ -1,17 +1,6 @@
 <?php
 include_once 'aa_test.php';
 ?>
-
-<div class="w-[98%]">
-
-    @php
-        //Appel direct au helper possible dans la Vue
-        $frameworksLinks = getGc7FrameworksLinks();
-        // dd($frameworksLinks);
-    @endphp
-    {{-- {{ dd($frameworksLinks) }} --}}
-    <div class="mx-[-.75rem] mt-[-1rem]">
-        <livewire:gc7.frameworks.livewire.serie7.03_offset />
-    </div>
-
+<div>
+    Ready.
 </div>
