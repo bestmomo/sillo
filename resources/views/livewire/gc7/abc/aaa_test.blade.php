203f05c6b5412edab4985d2d7cd938be0ac74404
GrCOTE7 <grcote7@gmail.com>
2024-06-27 14:17:49 +0200

    Feat: Set developing component in test page

--- resources/views/livewire/gc7/abc/aaa_test.blade.php
+++ resources/views/livewire/gc7/abc/aaa_test.blade.php
@@ -2,7 +2,7 @@
 include_once 'aa_test.php';
 ?>
 
-<div>
+<div class="w-[98%]">
 
     @php
         //Appel direct au helper possible dans la Vue
@@ -10,63 +10,8 @@ include_once 'aa_test.php';
         // dd($frameworksLinks);
     @endphp
     {{-- {{ dd($frameworksLinks) }} --}}
-    Tests
-    {{-- <livewire:gc7.abc.livewire.hello-world /> --}}
-    {{-- <livewire:gc7.abc.sqlite /> --}}
-
-
-    {{-- // --}}
-    {{-- <livewire:gc7.abc.frameworks /> --}}
-    {{-- // --}}
-    {{-- <livewire:gc7.abc.alpinejs.alpine /> --}}
-
-    {{-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx --}}
-    {{-- <x-partials.gc7.helpers.uuu name='Lionel'/> --}}
-    {{-- <x-partials.gc7.helpers.form-textarea :postId=$postId title='TheTitle' placeholder='ThePlaceHolder' cancelBtn=1 /> --}}
-
-    {{-- 
-    <x-partials.helpers.textareahelper postId=5 action='createComment' $title='Leave a comment' placeholder='Your comment' /> --}}
-
-    {{-- <livewire:helpers.form-textarea :postId=$postId id=222 action='updateComment' title='Update your comment' cancelBtn=True />
-    <livewire:helpers.form-textarea :postId=$postId id=333 action='createAnswer' title='Your answer' placeholder='Your answer' cancelBtn=True />
-    <livewire:helpers.form-textarea :postId=$postId id='444' action='updateAnswer' title='Update your comment' cancelBtn=True /> --}}
-    {{-- <livewire:gc7.abc.uuu lastname='Côte'/> --}}
-    {{-- <livewire:gc7.helpers.form-textarea /> --}}
-    {{-- <livewire:gc7.abc.alpine /> --}}
-
-    {{-- Appel counter --}}
-    {{-- Counter: --}}
-    {{-- <livewire:gc7.abc.mains.counter :num="777" :count="777" /> --}}
-    {{-- <x-helpers.form-textarea charsNbLeft="12" /> --}}
-
-    {{-- <livewire: gc7.abc.mains.counter :n=7 " /> --}}
-
-    {{-- <hr class="my-5"> --}}
-
-    {{-- <x-partials.abc.counterbox :num=777 :count=100 /> --}}
-    {{-- BEST SOLUTION
-    <div x-data="{ name: '{{ $name }}' }">
-        <x-input type="text" wire:model='name' x-on:input="name = $event.target.value" />
-        <p>Characters Count:<br>
-            <span x-text="name.length"></span>
-        </p>
-    </div> 
-    --}}
-    {{-- <div x-data="{ name: '{{ $name }}' }">
-        <x-input type="text" wire:model='name' x-on:input="name = $event.target.value" />
-        <p>Characters Count<br>
-            </span> <span x-text="name.length"></span> pour <span x-text="name">
-        </p>
+    <div class="mx-[-.75rem] mt-[-1rem]">
+        <livewire:gc7.frameworks.livewire.serie7.03_offset />
     </div>
-    <hr>
-    {{ $name }}
-    <hr>
-    {{ trans_choice('uuu :n', 10, ['n' => 100]) }}
-    <hr> --}}
-    @php
-        // $nb2 = env('APP_MAX_NUMBER_OF_CHARS_IN_COMMENTS_FORM', 700);
-    @endphp
-    {{-- <x-gc7.helpers.form-textarea :charsNbLeft= 55 /> --}}
-    {{-- <x-gc7.helpers.form-textarea :charsNbLeft= 77 /> --}}
-    {{-- <x-gc7.sql /> --}}
+
 </div>
--- resources/views/livewire/gc7/frameworks/livewire/serie7/03_offset.blade.php
+++ resources/views/livewire/gc7/frameworks/livewire/serie7/03_offset.blade.php
@@ -13,12 +13,32 @@ new class extends Component {
 }; ?>
 
 <div>
-    
+    @section('styles')
+        <style>
+            .drawer-content {
+                padding: 0;
+            }
+
+            .btn-menu {
+                color: #ccc;
+                transition: .5s ease-in-out;
+            }
+
+            .btn-menu.active {
+                font-weight: bold;
+                color: orange;
+            }
+
+            .btn-menu:hover {
+                color: #c7cf2f;
+            }
+        </style>
+    @endsection
     {{-- <x-header class="mb-0 pt-3" title="Série 7 - Offset" shadow separator progress-indicator /> --}}
-    <x-header class="mb-0 mt-[-12px]" shadow separator progress-indicator />
-    
-    <div class="px-3 mt-[-12px]">
-    <div class="py-1 px-5 bg-white dark:bg-gray-800 text-white rounded">
-        <p>Offset</p>
+    <x-header class="mt-[-12px] px-0 w-[96%] mx-auto" shadow separator progress-indicator />
+
+    <div class="px-5 mt-[-12px]">
+        <div class="py-1 px-3 bg-white dark:bg-gray-800 text-white rounded">
+            <p>Offset 21</p>
+        </div>
     </div>
-</div>
