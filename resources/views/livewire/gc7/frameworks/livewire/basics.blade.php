6d0e3dc62d0f55d39ed6495799eae1b144557ef3
GrCOTE7 <grcote7@gmail.com>
2024-06-27 14:23:34 +0200

    Feat: Basics component

--- resources/views/livewire/gc7/frameworks/livewire/basics.blade.php
+++ resources/views/livewire/gc7/frameworks/livewire/basics.blade.php
@@ -1,5 +1,26 @@
-        {{-- <livewire:gc7.abc.livewire.hello-world /> --}}
-        {{-- <livewire:gc7.abc.livewire.counter /> --}}
-        {{-- <livewire:gc7.abc.livewire.counter by=3/> --}}
+<?php
 
-        {{-- // 2do Basics opérationel --}}
+use Livewire\Volt\Component;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+
+new #[Title('Basics')] #[Layout('components.layouts.gc7.main')] class extends Component {
+    public $name = '';
+
+    public function mount()
+    {
+        $this->name = 'GC7';
+    }
+}; ?>
+
+<div>
+    <x-header class="pb-0 mb-[-14px]" title="Basics" shadow separator progress-indicator />
+    <h2>Hello Wold, <b>{{ $name }}</b>!</h2>
+    <hr>
+    The current time is {{ time() }} seconds since 1/1/1970.<br>
+    The complete current date is <b>{{ date('Y-m-d H:i:s', time()) }}</b>.
+    <hr>
+    <div class="text-right mt-3">
+        <x-button wire:click='$refresh' class='btn-primary'>Refresh</x-button>
+    </div>
+</div>
