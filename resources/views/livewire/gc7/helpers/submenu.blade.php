2cdb94cd7c677853fbb0ee9904ca7c92d943e4cf
GrCOTE7 <grcote7@gmail.com>
2024-07-03 08:26:44 +0200

    Feat: submenu includes factorised

--- resources/views/livewire/gc7/helpers/submenu.blade.php
+++ resources/views/livewire/gc7/helpers/submenu.blade.php
@@ -8,14 +8,14 @@ new class extends Component {
 
 <div x-data="{
     choice: null,
-    btns: {{ json_encode($btns) }},
+    btns: {{ json_encode($btns) }}
 }">
     <div class="w-full flex justify-evenly items-center mb-0 mt-[-27px] p-0">
 
         <template x-for="btn in btns" :key="btn">
 
             <button class='mr-3 btn btn-sm mb-0 pb-0' :class="choice !== btn ? 'btn-primary' : 'btn-secondary'"
-                x-on:click="choice = btn" :id="btn">
+                x-on:click="choice = btn; console.log('choice = ' + choice)" :id="btn">
                 <span x-text="btn"></span>
             </button>
 
@@ -27,42 +27,49 @@ new class extends Component {
         </p>
     </div>
 
-    {{-- <span x-text="choice"></span> --}}
-
     <hr class="mb-1">
+
+    {{-- Choice: <span x-text="choice"></span> --}}
 
     <div class="absolute" x-cloak x-transition.opacity.duration.777ms x-show="choice != 'V1' && choice !='V2'">
         <p>Choose v1 or v2, please !</p>
     </div>
 
-    <div class="absolute" x-cloak x-transition.opacity.duration.700ms x-show="choice == 'V1'">
-        @livewire('gc7.frameworks.alpinejs.chats.chat-v1')
-    </div>
+    <ul>
+        @foreach ($btns as $btn)
+            <li>
+                <div class="absolute" x-cloak x-transition.opacity.duration.700ms
+                    x-show="choice === '{{ $btn }}'">
+                    <div wire:key="chat-component-{{ $btn }}">
+                        @livewire('gc7.frameworks.alpinejs.chats.chat-' . strtolower($btn), key('chat-' . $btn))
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
 
-    <div class="absolute" x-cloak x-transition.opacity.duration.700ms x-show="choice == 'V2'">
-        @livewire('gc7.frameworks.alpinejs.chats.chat-v2')
-    </div>
+</div>
 
-    <script>
-        // Déclenchement automatique d'un onglet
-        window.onload = function() {
-            setTimeout(function() {
-                // Choix du bouton cliqué par défaut
-                let btn = 'Tabsuuu';
-                if (!btn) {
-                    console.log('Pas de bouton cliqué par défaut')
+<script>
+    // Déclenchement automatique d'un onglet
+    window.onload = function() {
+        setTimeout(function() {
+            // Choix du bouton cliqué par défaut
+            let btn = 'Tabsuuu';
+            if (!btn) {
+                console.log('Pas de bouton cliqué par défaut')
+            } else {
+
+                console.log('Je clique sur ' + btn);
+                const btnTabs = document.querySelector('button[id=' + btn + ']');
+                if (btnTabs) {
+                    btnTabs.click();
                 } else {
-
-                    console.log('Je clique sur ' + btn);
-                    const btnTabs = document.querySelector('button[id=' + btn + ']');
-                    if (btnTabs) {
-                        btnTabs.click();
-                    } else {
-                        console.log("Le bouton '" + btn + "' n'a pas été trouvé.");
-                    }
+                    console.log("Le bouton '" + btn + "' n'a pas été trouvé.");
                 }
-            }, 100); // Attend .1 seconde avant d'exécuter le script
-        };
-    </script>
+            }
+        }, 100); // Attend .1 seconde avant d'exécuter le script
+    };
+</script>
 
 </div>
