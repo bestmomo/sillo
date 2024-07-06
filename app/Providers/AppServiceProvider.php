9e9430e52c2f88f9198762a57e59afd61659bbf5
GrCOTE7 <grcote7@gmail.com>
2024-07-06 15:29:08 +0200

    Fix: ServiceProvider Setting only after table settings is there

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,34 +1,49 @@
 <?php
 
+/**
+ * (ɔ) LARAVEL.Sillo.org - 2015-2024
+ */
+
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\Menu;
+use App\Models\Setting;
 use Illuminate\View\View;
 use Illuminate\Support\Facades;
-
-use App\Models\Menu;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        Facades\View::composer(['components.layouts.app'], function (View $view) {
-            $view->with('menus', 
-                Menu::with(['submenus' => function ($query) {
-                    $query->orderBy('order');
-                }])->orderBy('order')->get()
-            );            
-        });
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		Facades\View::composer(['components.layouts.app'], function (View $view) {
+			$view->with(
+				'menus',
+				Menu::with(['submenus' => function ($query) {
+					$query->orderBy('order');
+				}])->orderBy('order')->get()
+			);
+		});
+
+		if (!$this->app->runningInConsole()) {
+			// Vérifiez si la table 'settings' existe
+			if (Schema::hasTable('settings')) {
+				$settings = Setting::all();
+				foreach ($settings as $setting) {
+					config(['app.' . $setting->key => $setting->value]);
+				}
+			}
+		}
+	}
 }
