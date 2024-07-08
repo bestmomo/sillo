84a4356bd190392ad43b2bc8ae7aed877ede5939
GrCOTE7 <grcote7@gmail.com>
2024-07-08 11:36:00 +0200

    Feat: Add MailHog

--- start.bat
+++ start.bat
@@ -10,3 +10,16 @@ echo Démarrage des serveurs.
 start /b npm run dev
 start /b php artisan reverb:start
 start /b php artisan serve
+
+
+@REM Start serveur de Mail
+start /b config/MailHog_windows_amd64.exe
+
+REM Envoi de l'email de Test
+@REM powershell.exe -ExecutionPolicy Bypass -File "testMailServer.ps1"
+@REM php send_email.php
+
+echo.
+echo Les serveurs sont démarrés.
+
+pause
