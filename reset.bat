74f4b4d34439da6f141b7301abb524fee3bab038
GrCOTE7 <grcote7@gmail.com>
2024-07-09 07:44:19 +0200

    Feat: Notice MailHog page

--- reset.bat
+++ reset.bat
@@ -140,10 +140,23 @@ call php artisan cache:clear
 call php artisan view:clear
 call php artisan config:clear
 
-
 echo.
-echo Nettoyage et processus terminé.
+echo Démarrage des serveurs.
 
 start /b npm run dev
 start /b php artisan reverb:start
 start /b php artisan serve
+
+
+@REM Start serveur de Mail
+@REM → http://localhost:8025/#
+start /b config/MailHog_windows_amd64.exe
+
+REM Envoi de l'email de Test
+@REM powershell.exe -ExecutionPolicy Bypass -File "testMailServer.ps1"
+@REM php send_email.php
+
+echo.
+echo Les serveurs sont démarrés.
+
+pause
