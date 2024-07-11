7e1844b459b5a200d877e0002bda008c1335e4de
GrCOTE7 <grcote7@gmail.com>
2024-07-11 21:01:55 +0200

    Feat Aca users shows students count by role

--- resources/views/livewire/academy/abc/aa_test.php
+++ resources/views/livewire/academy/abc/aa_test.php
@@ -13,6 +13,8 @@ new
 #[Layout('components.layouts.academy')]
 class() extends Component {
 	public $roleCounts = [];
+	public $studentCounts;
+	public $nbrStudents;
 	public $nbrUsers;
 
 	public function mount()
@@ -25,14 +27,16 @@ class() extends Component {
 		return [];
 	}
 
-	protected function usersStat()
-	{
-		$result = User::query()
-			->selectRaw('role, COUNT(*) as count')
-			->groupBy('role')
-			->get();
+protected function usersStat()
+{
+    $result = User::query()
+        ->selectRaw('role, COUNT(*) as count, SUM(CASE WHEN isStudent = true THEN 1 ELSE 0 END) as student_count')
+        ->groupBy('role')
+        ->get();
 
-		$this->roleCounts = $result->pluck('count', 'role');
-		$this->nbrUsers   = $result->sum('count');
-	}
+    $this->roleCounts = $result->pluck('count', 'role');
+    $this->studentCounts = $result->pluck('student_count', 'role');
+    $this->nbrUsers = $result->sum('count');
+    $this->nbrStudents = $result->sum('student_count');
+}
 };
--- resources/views/livewire/academy/abc/aaa_test.blade.php
+++ resources/views/livewire/academy/abc/aaa_test.blade.php
@@ -1,5 +1,5 @@
 <?php
-    include_once 'aa_test.php';
+include_once 'aa_test.php';
 ?>
 <div>
     <x-header class="mb-0" title="Test" shadow separator progress-indicator />
@@ -11,17 +11,25 @@
     <div class="my-3 font-bold text-xl">Users :</div>
 
     <table class="w-1/2 mx-auto">
-        @foreach ($roleCounts as $role => $count)
-            <tr class="!border-0">
-
-                <td class="border-b-gray-500 pl-3">{{ ucfirst($role) }}</td>
-                <td class="text-right border-b-gray-500 pr-7">{{ $count }}</td>
+        <thead>
+            <th class="rounded-tl-lg">Role</th>
+            <th>Student</th>
+            <th class="rounded-tr-lg">Total</th>
+        </thead>
+        <tbody>
+            @foreach ($roleCounts as $role => $count)
+                <tr class="!border-0">
+                    <td class="border-b-gray-500 pl-3">{{ ucfirst($role) }}</td>
+                    <td class="text-right border-b-gray-500 pr-7">{{ $studentCounts[$role] ?? 0 }}</td>
+                    <td class="text-right border-b-gray-500 pr-7">{{ $count }}</td>
+                </tr>
+            @endforeach
+            <tr>
+                <td class="border-0 font-bold"></td>
+                <td class="text-right font-bold border-0 pr-7">{{ $nbrStudents }}</td>
+                <td class="text-right font-bold border-0 pr-7">{{ $nbrUsers }}</td>
             </tr>
-        @endforeach
-        <tr>
-            <td class="border-0"></td>
-            <td class="text-right font-bold border-0 pr-7">{{ $nbrUsers }}</td>
-        </tr>
+        </tbody>
     </table>
 
     {{-- {{ $users->pluck('name', 'role')->join(', ') }} --}}
