ccdc7bcbf66c91bc11bc72b11063f81ccf7d0e25
GrCOTE7 <grcote7@gmail.com>
2024-07-15 16:00:32 +0200

    Fix: Reset original placeholder

--- resources/views/components/partials/academy/helpers/input.blade.php
+++ resources/views/components/partials/academy/helpers/input.blade.php
@@ -1,7 +1,7 @@
     <div class="field mb-5">
         <x-input 
             type="text" 
-            placeholder="Rechercher un membre" icon="o-magnifying-glass"
+            placeholder="{{ __('Search...') }}" icon="o-magnifying-glass"
             wire:model.live.debounce.300ms="search" 
             :clearable="!empty($this->search)"
              x-init="$nextTick(() => $el.focus())"
