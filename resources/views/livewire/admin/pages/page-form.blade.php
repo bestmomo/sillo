8b114d17599b080dd584fb745799fd20f658a9b6
bestmomo <colorstonic@free.fr>
2024-07-19 21:26:26 +0200

    Code refactoring

---
+++ resources/views/livewire/admin/pages/page-form.blade.php
@@ -0,0 +1,23 @@
+<x-card>
+    <x-form wire:submit="save">
+        <x-input type="text" wire:model="title" label="{{ __('Title') }}"
+            placeholder="{{ __('Enter the title') }}" wire:change="$refresh" />
+        <x-input type="text" wire:model="slug" label="{{ __('Slug') }}" />
+        <x-editor wire:model="body" label="{{ __('Content') }}" :config="config('tinymce.config')"
+            folder="{{ 'photos/' . now()->format('Y/m') }}" />
+        <x-card title="{{ __('SEO') }}" shadow separator>
+            <x-input placeholder="{{ __('Title') }}" wire:model="seo_title" hint="{{ __('Max 70 chars') }}" />
+            <br>
+            <x-textarea label="{{ __('META Description') }}" wire:model="meta_description"
+                hint="{{ __('Max 160 chars') }}" rows="2" inline />
+            <br>
+            <x-textarea label="{{ __('META Keywords') }}" wire:model="meta_keywords"
+                hint="{{ __('Keywords separated by comma') }}" rows="1" inline />
+        </x-card>
+        <x-slot:actions>
+            <x-button label="{{ __('Save') }}" icon="o-paper-airplane" spinner="save" type="submit"
+                class="btn-primary" />
+        </x-slot:actions>
+    </x-form>
+</x-card>
+
--- resources/views/livewire/admin/quizzes/edit.blade.php
+++ resources/views/livewire/admin/quizzes/edit.blade.php
@@ -76,54 +76,5 @@ new #[Title('Edit Quiz'), Layout('components.layouts.admin')] class extends Comp
                 link="{{ route('admin') }}" />
         </x-slot:actions>
     </x-header>
-    <x-card>
-        <x-form wire:submit.prevent="save">
-            <x-input type="text" wire:model="title" label="{{ __('Title') }}"
-                placeholder="{{ __('Enter the title') }}" />
-            <x-input type="text" wire:model="description" label="{{ __('Description') }}"
-                placeholder="{{ __('Enter the description') }}" />
-            <x-choices label="{{ __('Post') }}" wire:model="post_id" :options="$postsSearchable" option-label="title"
-                hint="{{ __('Select a post or none, type to search') }}" debounce="300ms" min-chars="2"
-                no-result-text="{{ __('No result found!') }}" single searchable />
-            @foreach ($questions as $qIndex => $question)
-                <hr>
-                <div class="flex flex-row justify-between">
-                    <x-badge value="Question {{ $qIndex + 1 }}" class="p-4 badge-accent" />
-                    @if ($qIndex > 1)
-                        <x-button label="{{ __('Remove Question') }}"
-                            wire:click.prevent="removeQuestion({{ $qIndex }})" class="btn-warning" />
-                    @endif
-                </div>
-                <x-input type="text" wire:model="questions.{{ $qIndex }}.question_text"
-                    label="{{ __('Question') }}" placeholder="{{ __('Enter the question text') }}" />
-                <hr>
-
-                @foreach ($question['answers'] as $aIndex => $answer)
-                    <x-input type="text"
-                        wire:model="questions.{{ $qIndex }}.answers.{{ $aIndex }}.answer_text"
-                        label="{{ __('Answer ') }} {{ $aIndex + 1 }}"
-                        placeholder="{{ __('Enter the answer text') }}" />
-                    <x-checkbox wire:model="questions.{{ $qIndex }}.answers.{{ $aIndex }}.is_correct"
-                        label="{{ __('Is Correct') }}" />
-                    @if ($aIndex > 1)
-                        <x-button label="{{ __('Remove Answer') }} {{ $aIndex + 1 }}"
-                            wire:click.prevent="removeAnswer({{ $qIndex }}, {{ $aIndex }})"
-                            class="btn-warning" />
-                    @else
-                        <hr>
-                    @endif
-                @endforeach
-
-                <x-button label="{{ __('Add Answer') }}" wire:click.prevent="addAnswer({{ $qIndex }})"
-                    class="btn-primary" />
-            @endforeach
-
-            <x-button label="{{ __('Add Question') }}" wire:click.prevent="addQuestion" class="btn-info" />
-
-            <x-slot:actions>
-                <x-button label="{{ __('Save') }}" icon="o-paper-airplane" spinner="save" type="submit"
-                    class="btn-primary" />
-            </x-slot:actions>
-        </x-form>
-    </x-card>
+    @include('livewire.admin.quizzes.quiz-form')
 </div>
