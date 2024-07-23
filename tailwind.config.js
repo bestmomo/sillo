b74f6a0979698c6e92e7aeefb015ec312731661a
bestmomo <colorstonic@free.fr>
2024-07-23 15:13:56 +0200

    Update Vite and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,24 +12,10 @@ export default {
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
   ],
   safelist: [
+    {
+      pattern: /bg-(red|green|yellow|blue|indigo|purple|pink|orange|teal|lime|cyan|amber|fuchsia|emerald|rose|sky|violet)-200/,
+    },
     'bg-red-300',
-    'bg-red-200',
-    'bg-green-200',
-    'bg-yellow-200',
-    'bg-blue-200',
-    'bg-indigo-200',
-    'bg-purple-200',
-    'bg-pink-200',
-    'bg-orange-200',
-    'bg-teal-200',
-    'bg-lime-200',
-    'bg-cyan-200',
-    'bg-amber-200',
-    'bg-fuchsia-200',
-    'bg-emerald-200',
-    'bg-rose-200',
-    'bg-sky-200',
-    'bg-violet-200', 
   ],
   theme: {
     extend: {
