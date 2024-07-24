8017cdd4bc75fa242383ef266ccd3c13496c53ab
bestmomo <colorstonic@free.fr>
2024-07-24 13:54:16 +0200

    Add firstname in register and format code
    I'm not very keen on gender in user data

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -6,65 +6,70 @@ use Livewire\Attributes\{Layout, Rule, Title};
 use Livewire\Volt\Component;
 
 // Définition du composant avec les attributs de titre et de mise en page
-new
-#[Title('Register')]
-#[Layout('components.layouts.auth')]
-class extends Component {
-	// Définition des règles de validation pour les champs du formulaire
-	#[Rule('required|string|max:255|unique:users')]
-	public string $name = '';
+new #[Title('Register')] #[Layout('components.layouts.auth')] class extends Component {
+    // Définition des règles de validation pour les champs du formulaire
+    #[Rule('required|string|max:255|unique:users')]
+    public string $name = '';
 
-	#[Rule('required|email|unique:users')]
-	public string $email = '';
+    #[Rule('required|string|max:255')]
+    public string $firstname = '';
 
-	#[Rule('required|confirmed')]
-	public string $password = '';
+    #[Rule('required|email|unique:users')]
+    public string $email = '';
 
-	#[Rule('required')]
-	public string $password_confirmation = '';
+    #[Rule('required|confirmed')]
+    public string $password = '';
 
-	// Méthode pour enregistrer un nouvel utilisateur
-	public function register()
-	{
-		// Validation des données
-		$data = $this->validate();
+    #[Rule('required')]
+    public string $password_confirmation = '';
 
-		// Hashage du mot de passe
-		$data['password'] = Hash::make($data['password']);
+    // Méthode pour enregistrer un nouvel utilisateur
+    public function register()
+    {
+        // Validation des données
+        $data = $this->validate();
 
-		// Création de l'utilisateur
-		$user = User::create($data);
+        // Hashage du mot de passe
+        $data['password'] = Hash::make($data['password']);
 
-		// Authentification de l'utilisateur
-		auth()->login($user);
+        // Création de l'utilisateur
+        $user = User::create($data);
 
-		// Régénération de la session
-		request()->session()->regenerate();
+        // Authentification de l'utilisateur
+        auth()->login($user);
 
-		// Redirection vers la page d'accueil
-		return redirect('/');
-	}
+        // Régénération de la session
+        request()->session()->regenerate();
+
+        // Redirection vers la page d'accueil
+        return redirect('/');
+    }
 }; ?>
 
 <div>
     <!-- Formulaire d'inscription -->
-    <x-card class="flex items-center justify-center h-screen" title="{{__('Register')}}" shadow separator progress-indicator>
- 
+    <x-card class="flex items-center justify-center h-screen" title="{{ __('Register') }}" shadow separator
+        progress-indicator>
+
         <x-form wire:submit="register">
             <!-- Champ de nom -->
-            <x-input label="{{__('Name')}}" wire:model="name" icon="o-user" inline />
+            <x-input label="{{ __('Name') }}" wire:model="name" icon="o-user" inline />
+            <!-- Champ de prénom -->
+            <x-input label="{{ __('Firstname') }}" wire:model="firstname" icon="o-user" inline />
             <!-- Champ d'email -->
-            <x-input label="{{__('E-mail')}}" wire:model="email" icon="o-envelope" inline />
+            <x-input label="{{ __('E-mail') }}" wire:model="email" icon="o-envelope" inline />
             <!-- Champ de mot de passe -->
-            <x-input label="{{__('Password')}}" wire:model="password" type="password" icon="o-key" inline />
+            <x-input label="{{ __('Password') }}" wire:model="password" type="password" icon="o-key" inline />
             <!-- Champ de confirmation du mot de passe -->
-            <x-input label="{{__('Confirm Password')}}" wire:model="password_confirmation" type="password" icon="o-key" inline />
-    
+            <x-input label="{{ __('Confirm Password') }}" wire:model="password_confirmation" type="password"
+                icon="o-key" inline />
+
             <x-slot:actions>
                 <!-- Bouton pour rediriger vers la page de connexion -->
-                <x-button label="{{__('Already registered?')}}" class="btn-ghost" link="/login" />
+                <x-button label="{{ __('Already registered?') }}" class="btn-ghost" link="/login" />
                 <!-- Bouton pour soumettre le formulaire -->
-                <x-button label="{{__('Register')}}" type="submit" icon="o-paper-airplane" class="btn-primary" spinner="login" />
+                <x-button label="{{ __('Register') }}" type="submit" icon="o-paper-airplane" class="btn-primary"
+                    spinner="login" />
             </x-slot:actions>
         </x-form>
 
