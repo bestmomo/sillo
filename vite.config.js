d6b43c9e00ed13a44a0fa41affc652fb95df809f
bestmomo <colorstonic@free.fr>
2024-07-24 18:07:38 +0200

    Fix settings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
   plugins: [
     laravel({
-      input: ["resources/css/app.css", "resources/js/app.js"],
+      input: ["resources/css/app.css", 'resources/css/academy-styles.css', "resources/js/app.js"],
       refresh: [
         ".env",
         "public/assets/**",
