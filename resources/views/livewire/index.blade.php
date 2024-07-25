ff07ca792f702f5c385855bfd5d1c885ff7aa8ac
bestmomo <colorstonic@free.fr>
2024-07-26 00:34:05 +0200

    Adapt style for small screens

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -17,7 +17,6 @@ new class extends Component {
     public bool $favorites = false;
     public Collection $surveys;
 
-
     /**
      * Méthode de montage initiale appelée lors de la création du composant.
      *
@@ -156,79 +155,84 @@ new class extends Component {
     </div>
 
     <!-- Liste des posts -->
-    <div class="grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 lg:grid-cols-3">
-        @forelse($posts as $post)
-            <x-card
-                class="transition duration-500 ease-in-out shadow-md shadow-gray-500 hover:shadow-xl hover:shadow-gray-500"
-                title="{{ $post->title }}">
-
-                <div class="text-justify">{!! str($post->excerpt)->words(config('app.excerptSize')) !!}</div>
-                <br>
-                <hr>
-                <div class="flex justify-between">
-                    <p wire:click="" class="text-left cursor-pointer">{{ $post->user->name }}</p>
-                    <p class="text-right"><em>{{ $post->created_at->isoFormat('LL') }}</em></p>
-                </div>
-                <x-slot:figure>
-                    <a href="{{ url('/posts/' . $post->slug) }}">
-                        <img src="{{ asset('storage/photos/' . $post->image) }}" alt="{{ $post->title }}" />
-                    </a>
-                </x-slot:figure>
-
-                <x-slot:menu>
-                    @if ($post->pinned)
-                        <x-badge value="{{ __('Pinned') }}" class="p-3 badge-warning" />
-                    @elseif($post->created_at->gt(now()->subWeeks(config('app.newPost'))))
-                        <x-badge value="{{ __('New') }}" class="p-3 badge-success" />
-                    @endif
-                    @auth
-                        @if ($post->is_favorited)
-                            <x-icon name="s-star" class="w-6 h-6 text-yellow-500 cursor-pointer" />
+    <div class="container px-4 mx-auto">
+        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            @forelse($posts as $post)
+                <x-card
+                    class="w-full transition duration-500 ease-in-out shadow-md shadow-gray-500 hover:shadow-xl hover:shadow-gray-500"
+                    title="{{ $post->title }}">
+    
+                    <div class="text-justify">{!! str($post->excerpt)->words(config('app.excerptSize')) !!}</div>
+                    <br>
+                    <hr>
+                    <div class="flex justify-between">
+                        <p wire:click="" class="text-left cursor-pointer">{{ $post->user->name }}</p>
+                        <p class="text-right"><em>{{ $post->created_at->isoFormat('LL') }}</em></p>
+                    </div>
+                    <x-slot:figure>
+                        <a href="{{ url('/posts/' . $post->slug) }}">
+                            <img src="{{ asset('storage/photos/' . $post->image) }}" alt="{{ $post->title }}" />
+                        </a>
+                    </x-slot:figure>
+    
+                    <x-slot:menu>
+                        @if ($post->pinned)
+                            <x-badge value="{{ __('Pinned') }}" class="p-3 badge-warning" />
+                        @elseif($post->created_at->gt(now()->subWeeks(config('app.newPost'))))
+                            <x-badge value="{{ __('New') }}" class="p-3 badge-success" />
                         @endif
-                    @endauth
-                </x-slot:menu>
-
-                <x-slot:actions class="flex items-center">
-                    <x-popover>
-                        <x-slot:trigger>
-                            <x-button label="{{ $post->category->title }}"
-                                link="{{ url('/category/' . $post->category->slug) }}" class="btn-outline btn-sm" />
-                        </x-slot:trigger>
-                        <x-slot:content class="pop-small">
-                            @lang('Show this category')
-                        </x-slot:content>
-                    </x-popover>
-
-                    @if ($post->serie)
-                        <x-popover>
-                            <x-slot:trigger>
-                                <x-button label="{{ $post->serie->title }}"
-                                    link="{{ url('/serie/' . $post->serie->slug) }}" class="btn-outline btn-sm" />
-                            </x-slot:trigger>
-                            <x-slot:content class="pop-small">
-                                @lang('Show this serie')
-                            </x-slot:content>
-                        </x-popover>
-                    @endif
-                    <x-popover>
-                        <x-slot:trigger>
-                            <x-button label="{{ __('Read') }}" link="{{ url('/posts/' . $post->slug) }}"
-                                class="btn-outline btn-sm" />
-                        </x-slot:trigger>
-                        <x-slot:content class="pop-small">
-                            @lang('Read this post')
-                        </x-slot:content>
-                    </x-popover>
-                </x-slot:actions>
-            </x-card>
-        @empty
-            <div class="col-span-3">
-                <x-card title="{{ __('Nothing to show !') }}">
-                    {{ __('No Post found with these criteria') }}
+                        @auth
+                            @if ($post->is_favorited)
+                                <x-icon name="s-star" class="w-6 h-6 text-yellow-500 cursor-pointer" />
+                            @endif
+                        @endauth
+                    </x-slot:menu>
+    
+                    <x-slot:actions>
+                        <div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-2">
+                            <x-popover>
+                                <x-slot:trigger>
+                                    <x-button label="{{ $post->category->title }}"
+                                        link="{{ url('/category/' . $post->category->slug) }}" class="btn-outline btn-sm" />
+                                </x-slot:trigger>
+                                <x-slot:content class="pop-small">
+                                    @lang('Show this category')
+                                </x-slot:content>
+                            </x-popover>
+        
+                            @if ($post->serie)
+                                <x-popover>
+                                    <x-slot:trigger>
+                                        <x-button label="{{ $post->serie->title }}"
+                                            link="{{ url('/serie/' . $post->serie->slug) }}" class="btn-outline btn-sm" />
+                                    </x-slot:trigger>
+                                    <x-slot:content class="pop-small">
+                                        @lang('Show this serie')
+                                    </x-slot:content>
+                                </x-popover>
+                            @endif
+                            <x-popover>
+                                <x-slot:trigger>
+                                    <x-button label="{{ __('Read') }}" link="{{ url('/posts/' . $post->slug) }}"
+                                        class="btn-outline btn-sm" />
+                                </x-slot:trigger>
+                                <x-slot:content class="pop-small">
+                                    @lang('Read this post')
+                                </x-slot:content>
+                            </x-popover>
+                        </div>
+                    </x-slot:actions>
                 </x-card>
-            </div>
-        @endforelse
+            @empty
+                <div class="col-span-3">
+                    <x-card title="{{ __('Nothing to show !') }}">
+                        {{ __('No Post found with these criteria') }}
+                    </x-card>
+                </div>
+            @endforelse
+        </div>
     </div>
+    
 
     <!-- Pagination inférieure -->
     <div class="mb-4 mary-table-pagination">
