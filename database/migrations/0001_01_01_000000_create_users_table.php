3ce3f5d4d58c7b112c110aa25447378ba47b419b
bestmomo <colorstonic@free.fr>
2024-07-31 17:17:55 +0200

    Name is unique

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class() extends Migration {
 	{
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
-			$table->string('name');
+			$table->string('name')->unique();
 			$table->string('email')->unique();
 			$table->string('password');
 			$table->enum('role', ['user', 'redac', 'admin'])->default('user');
