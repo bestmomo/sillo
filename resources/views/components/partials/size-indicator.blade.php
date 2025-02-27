fdb3773595c2e54f8ed30c774c700e2da0973cf8
GrCOTE7 <grcote7@gmail.com>
2025-02-27 09:26:12 +0100

    Fix: To avoid errtor if 2 or more calls to Screen size-indicator

--- resources/views/components/partials/size-indicator.blade.php
+++ resources/views/components/partials/size-indicator.blade.php
@@ -8,10 +8,12 @@ $breakpoints = ['xs', 'sm', 'md', 'lg', 'xl', '2xl'];
 
 $code = "<div style=\"z-index: 99999999\" class='fixed bottom-12 right-5 opacity-50 border p-1 rounded-lg bg-black'>Taille écran<div class=\"text-yellow-500 text-4xl z-10 font-bold text-center\">\n";
 
-function ind()
-{
-    // indentation
-    return str_repeat(' ', 4);
+if (!function_exists('ind')) {
+    function ind()
+    {
+        // indentation
+        return str_repeat(' ', 4);
+    }
 }
 
 $brks = [];
